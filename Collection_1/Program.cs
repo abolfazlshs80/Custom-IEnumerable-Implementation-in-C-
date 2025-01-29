1f069354596f2d2ff9b381404e92d65b07763644
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-01-29 20:30:16 +0330

    Add project files.

---
+++ Collection_1/Program.cs
@@ -0,0 +1,59 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Collections;
+
+Console.WriteLine("Hello, World!");
+MyCustomList mylist = new MyCustomList();
+var list = mylist.GetEnumerator();
+while (list.MoveNext())
+{
+    int a =(int) list.Current;
+    Console.WriteLine(a);
+}
+
+
+
+
+
+
+public class MyCustomList : IEnumerable
+{
+
+    public IEnumerator GetEnumerator()
+    {
+        return new SingleSearch(new int[] { 1, 2, 3, 4, 5 });
+    }
+}
+
+public class SingleSearch : IEnumerator
+{
+    public int[] numbers;
+    private int index = -1;
+
+    public SingleSearch(int[] numbers)
+    {
+        this.numbers = numbers;
+    }
+    public bool MoveNext()
+    {
+        if (index==numbers.Length-1)
+        {
+            return false;
+        }
+        else
+        {
+            index++;
+            return true;
+        }
+    }
+
+    public void Reset()
+    {
+       index= -1;
+    }
+
+    public object Current
+    {
+        get { return numbers[index]; }
+    }
+}
